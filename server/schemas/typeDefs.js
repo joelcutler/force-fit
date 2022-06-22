5aa719b70df2763b5234337c7e99bc3a7078a572
Logan Parke <logan.c.parke@gmail.com>
2022-06-21 20:07:18 -0600

    typedef types

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,20 +1,30 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Categoty {
-
+  type Category {
+    _id: ID
+    name: String
   }
 
   type Excercise {
-
+    _id: ID
+    name: String
+    equipment: String
+    category: Category
   }
 
   type User {
-
+    firstName: String
+    lastName: String
+    email: String
+    password: String
+    Workouts: Workout
   }
 
+  # workout type needs fixing
   type Workout {
-
+    exercises: [Exercise]
+    day: Date
   }
 
   type Query {
@@ -22,7 +32,7 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    
+
   }
 `;
 
