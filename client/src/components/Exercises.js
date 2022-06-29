d6fae86fa235a9a88c1c8b21f5015de823249c66
Joel C <joel7921111@yahoo.com>
2022-06-29 16:33:03 -0600

    today's workout h2 on today component

--- client/src/components/Exercises.js
+++ client/src/components/Exercises.js
@@ -6,9 +6,8 @@ import { useStoreContext } from "../utils/GlobalState";
 import defaultExercises from "../assets/defaultExercises.json";
 import { parse } from "graphql";
 import defaultCategories from "../assets/defaultCategories.json";
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 import { SET_USER, SET_EXERCISE } from "../utils/actions";
-
 
 const Exercises = () => {
   // const { data: userData, loading, error, refetch } = useQuery(QUERY_USER);
@@ -18,17 +17,15 @@ const Exercises = () => {
 
   const [state, dispatch] = useStoreContext();
   let activeWorkout;
-  if(state.workout){
+  if (state.workout) {
     activeWorkout = state.workout._id;
     //console.log(activeWorkout)
   }
   //console.log(state);
 
-  
   const { loading, data } = useQuery(QUERY_USER);
 
   //console.log(activeWorkout);
-
 
   const defExs = defaultExercises.defaultExercises;
 
@@ -36,7 +33,7 @@ const Exercises = () => {
 
   const [addExerciseToWorkout] = useMutation(ADD_EXERCISE);
 
-  const [exerciseFormState, setExerciseFormState] = useState()
+  const [exerciseFormState, setExerciseFormState] = useState();
   // {workoutId: activeWorkout}
   const handleFormChange = (event) => {
     const { name, value } = event.target;
@@ -57,7 +54,7 @@ const Exercises = () => {
       });
     }
   }, [data, loading, dispatch]);
-  
+
   // THIS FUNCTION IS NOT WORKOING SOMEWHRE FRONT END OR BACKEND
   const addExercise = async (event) => {
     event.preventDefault();
@@ -70,24 +67,23 @@ const Exercises = () => {
           weight: parseInt(exerciseFormState.weight),
           sets: parseInt(exerciseFormState.sets),
           reps: parseInt(exerciseFormState.reps),
-          workoutId: activeWorkout
+          workoutId: activeWorkout,
         },
       });
-      if(data){
-      dispatch({
-        type: SET_EXERCISE,
-        user: addedEx
-      })
-    }
-
-    } catch (e){
+      if (data) {
+        dispatch({
+          type: SET_EXERCISE,
+          user: addedEx,
+        });
+      }
+    } catch (e) {
       console.log(e);
     }
   };
   //if (Auth.loggedIn()) {
   return (
     <div className="cards">
-      <h3 className="card-title">Exercises</h3>
+      <h3 className="card-title">Add Your Exercises</h3>
       {/* <div>
         {defExs && (
           <>
@@ -108,69 +104,106 @@ const Exercises = () => {
       {/* </> */}
       {/* )} */}
       {/* </div>  */}
-      
-  <div className="wrapper">
-    <div className="ui container exercise">
-      <div className="ui raised card m-auto">
-        <h2>Add Your Exercise</h2>
-        <div>
-          <div className="workout-type">
-            <label>Exercise Type:</label>
-            {/* <select name="type" id="type" defaultValue="Select Exercise Type">
+
+      <div className="wrapper">
+        <div className="ui container exercise">
+          <div className="ui raised card m-auto">
+            {/* <h2>Add Your Exercise</h2> */}
+            <div>
+              <div className="workout-type">
+                <label>Exercise Type:</label>
+                {/* <select name="type" id="type" defaultValue="Select Exercise Type">
             {defCats.map((categories) => (<option  key={categories._id} value={categories.categoryName}>{categories.categoryName}</option>))}
             </select> */}
-          </div>
-          <div className="cardio-form">
-            <div className="cardio-name">
-              <label>Name:</label>
-              <input onChange={handleFormChange} type="text" name="exerciseName" id="cardio-name" placeholder="Running" />
-            </div>
-            <div className="distance">
-              <label>Distance (miles):</label>
-              <input onChange={handleFormChange} type="number" name="distance" id="distance" placeholder="5" />
-            </div>
-            <div className="duration">
-              <label>Duration (minutes):</label>
-              <input onChange={handleFormChange} type="number" name="duration" id="duration" placeholder="10" />
-            </div>
-          </div>
-          <div className="resistance-form">
-            <div className="weight">
-              <label>Weight (lbs):</label>
-              <input onChange={handleFormChange} type="number" name="weight" id="weight" placeholder="200" />
-            </div>
-            <div className="sets">
-              <label>Sets:</label>
-              <input onChange={handleFormChange} type="number" name="sets" id="sets" placeholder="4" />
-            </div>
-            <div className="reps">
-              <label>Reps:</label>
-              <input onChange={handleFormChange} type="number" name="reps" id="reps" placeholder="10" />
-            </div>
-            {/* <div className="resistance-duration">
+              </div>
+              <div className="cardio-form">
+                <div className="cardio-name">
+                  <label>Name:</label>
+                  <input
+                    onChange={handleFormChange}
+                    type="text"
+                    name="exerciseName"
+                    id="cardio-name"
+                    placeholder="Running"
+                  />
+                </div>
+                <div className="distance">
+                  <label>Distance (miles):</label>
+                  <input
+                    onChange={handleFormChange}
+                    type="number"
+                    name="distance"
+                    id="distance"
+                    placeholder="5"
+                  />
+                </div>
+                <div className="duration">
+                  <label>Duration (minutes):</label>
+                  <input
+                    onChange={handleFormChange}
+                    type="number"
+                    name="duration"
+                    id="duration"
+                    placeholder="10"
+                  />
+                </div>
+              </div>
+              <div className="resistance-form">
+                <div className="weight">
+                  <label>Weight (lbs):</label>
+                  <input
+                    onChange={handleFormChange}
+                    type="number"
+                    name="weight"
+                    id="weight"
+                    placeholder="200"
+                  />
+                </div>
+                <div className="sets">
+                  <label>Sets:</label>
+                  <input
+                    onChange={handleFormChange}
+                    type="number"
+                    name="sets"
+                    id="sets"
+                    placeholder="4"
+                  />
+                </div>
+                <div className="reps">
+                  <label>Reps:</label>
+                  <input
+                    onChange={handleFormChange}
+                    type="number"
+                    name="reps"
+                    id="reps"
+                    placeholder="10"
+                  />
+                </div>
+                {/* <div className="resistance-duration">
               <label>Duration (minutes):</label>
               <input onChange={handleFormChange} type="number" name="resistance-duration" id="resistance-duration" placeholder="10" />
             </div> */}
-          </div>
-          <div className="buttons">
-            {/* <button>
+              </div>
+              <div className="buttons text-center">
+                {/* <button>
               Complete
             </button> */}
-            <button onClick={addExercise} className="bg-cyan-400">
-              Add Exercise
-              setExerciseFormState([...exerciseFormState, {}])
-            </button>
-
+                <button
+                  onClick={addExercise}
+                  className="bg-cyan-400 rounded-xl w-1/2 "
+                >
+                  Add Exercise
+                </button>
+                {/* setExerciseFormState([...exerciseFormState, {}]) */}
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    </div>
-    </div>
-  
   );
-          //}
-          //return <div>NOT logged in</div>
+  //}
+  //return <div>NOT logged in</div>
 };
 
 export default Exercises;
